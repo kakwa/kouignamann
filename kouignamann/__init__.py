c1e5ac3c1e63df9bff37b0b332f3995e6aaffc2c
pcarpent <pcarpent@e5400.si.c-s.fr>
2015-02-06 18:59:14 +0100

    class structure

--- kouignamann/__init__.py
+++ kouignamann/__init__.py
@@ -1,27 +1,44 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+from os import walk
+
+class YamlLoad():
+    def __init__(self, inventory_dir):
+        
+
 
 class Partitionings:
+    self.subdir = 'partitioning'
     def __init__(self, inventory_dir):
         pass
 
 class VirtualIps:
+    self.subdir = 'virtual-ips'
     def __init__(self, inventory_dir):
         pass
 
 class Hosts:
+    self.subdir = 'hosts'
     def __init__(self, inventory_dir):
         pass
 
 class Hardwares:
+    self.subdir = 'hardwares'
     def __init__(self, inventory_dir):
         pass
 
 
 class General:
+    self.subdir = 'general'
     def __init__(self, inventory_dir):
         pass
 
+class Inventory:
+    def __init__(self, inventory_dir):
+        self.hosts          = Hosts(inventory_dir)
+        self.general        = General(inventory_dir)
+        self.hardwares      = Hardwares(inventory_dir)
+        self.partitionings  = Partitionings(inventory_dir)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
